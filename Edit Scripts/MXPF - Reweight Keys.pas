63cb4146061b9089e94a35eb74155f1a8928dfa2
matortheeternal <why101@msn.com>
2016-01-23 12:30:23 -0800

    Added a new sample script using the QuickMXPF macro to reweight keys

---
+++ Edit Scripts/MXPF - Reweight Keys.pas
@@ -0,0 +1,25 @@
+{
+  MXPF - Reweight Keys
+  by matortheeternal
+  
+  Sample MXPF Script, sets Key record weights to 0.05.
+  
+  This script makes use of the QuickMXPF macro, which allows 
+  it to work in 3 lines of code.
+}
+
+unit UserScript;
+
+uses 'lib\mxpf';
+
+function Initialize: Integer;
+var
+  i: integer;
+begin
+  QuickMXPF('MXPF - Reweight Keys', mxHardcodedDatFiles, 'KEYM');
+  for i := MaxPatchRecordIndex downto 0 do 
+    seev(GetPatchRecord(i), 'DATA\Weight', '0.05');
+  FinalizeMXPF;
+end;
+
+end.
