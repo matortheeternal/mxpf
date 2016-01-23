cd9e7ec5b73b071e82b6474b0c878c2dab0fbf43
matortheeternal <why101@msn.com>
2016-01-23 13:00:32 -0800

    Fixed missing rec variable in Save Female NPCs script and added a few comments

--- Edit Scripts/MXPF - Save female NPCs.pas
+++ Edit Scripts/MXPF - Save female NPCs.pas
@@ -1,3 +1,12 @@
+{
+  MXPF - Save Female NPCs
+  by matortheeternal
+  
+  Sample MXPF script which saves a list of female NPCs
+  from a user-selected list of a files to a text document
+  in the same directory as TES5Edit.exe.
+}
+
 unit UserScript;
 
 uses 'lib\mxpf';
@@ -6,6 +15,7 @@ function Initialize: Integer;
 var
   i: integer;
   sl, files: TStringList;
+  rec: IInterface;
 begin
   // initialize stringlists
   sl := TStringList.Create;
@@ -13,10 +23,14 @@ begin
   
   // get user file selection
   MultiFileSelect(files, 'Select the files you want to load Female NPCs from');
+  
+  // use MXPF to load NPC_ records from the user's file selection
   InitializeMXPF;
   DefaultOptionsMXPF;
   SetInclusions(files.CommaText);
   LoadRecords('NPC_');
+  
+  // add names of female NPCs to the stringlist
   for i := 0 to MaxRecordIndex do begin
     rec := GetRecord(i);
     if geev(rec, 'ACBS/Flags/Female') = '1' then
